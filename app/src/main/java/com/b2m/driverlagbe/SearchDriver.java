ae76b3c0a278df65444ae9412b60397c7c4515ff
CZ-PC <md.prince3108@gmail.com>
2020-03-10 10:19:42 +0600

    Take New Activity then design your own part..

---
+++ app/src/main/java/com/b2m/driverlagbe/SearchDriver.java
@@ -0,0 +1,14 @@
+package com.b2m.driverlagbe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+    }
+}
