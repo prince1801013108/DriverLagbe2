fe56f78d4ff69f087077665ea3f0f6a06ed8ad6e
CZ-PC <md.prince3108@gmail.com>
2020-03-11 15:13:45 +0600

    Take New Activity then design your own part..

--- app/src/main/java/com/b2m/driverlagbe/Verification.java
+++ app/src/main/java/com/b2m/driverlagbe/Verification.java
@@ -3,19 +3,26 @@ package com.b2m.driverlagbe;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Verification extends AppCompatActivity {
+
+    EditText et1,et2,et3,et4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
 
-        final PinEntryEditText txtPinEntry = (PinEntryEditText) findViewById(R.id.txt_pin_entry);
-        txtPinEntry.addTextChangedListener(new TextWatcher() {
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        et3 = findViewById(R.id.et3);
+        et4 = findViewById(R.id.et4);
+
+
+        et1.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -28,11 +35,85 @@ public class Verification extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (s.toString().equals("1234")) {
-                    Toast.makeText(Verification.this, "Success", Toast.LENGTH_SHORT).show();
-                } else if (s.length() == "1234".length()) {
-                    Toast.makeText(Verification.this, "Incorrect", Toast.LENGTH_SHORT).show();
-                    txtPinEntry.setText(null);
+                if(s.length()==1)
+                {
+                    et2.requestFocus();
+                }
+                else if(s.length()==0)
+                {
+                    et1.clearFocus();
+                }
+            }
+        });
+
+        et2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()==1)
+                {
+                    et3.requestFocus();
+                }
+                else if(s.length()==0)
+                {
+                    et1.requestFocus();
+                }
+            }
+        });
+
+        et3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()==1)
+                {
+                    et4.requestFocus();
+                }
+                else if(s.length()==0)
+                {
+                    et2.requestFocus();
+                }
+            }
+        });
+
+        et4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()==1)
+                {
+                    et4.clearFocus();
+                }
+                else if(s.length()==0)
+                {
+                    et3.requestFocus();
                 }
             }
         });
